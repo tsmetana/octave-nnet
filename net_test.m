139d8726b5b523ff7526bb1bf341408c9335cf1a
Tomas Smetana <tsmetana@redhat.com>
2016-03-04 13:37:44 +0100

    finish the testing function

--- net_test.m
+++ net_test.m
@@ -3,12 +3,18 @@ function [retval] = net_test (net, ifile)
   fr = fopen(ifile);
   filedesc = str2num(fgetl(fr));
   # filedesc is vector of [number_of_examples input_dimension output_dimension]
+  err = 0
   for i = 1:filedesc(1)
-    in_val(i,:) = str2num(fgetl(fr));
-    out_val(i,:) = str2num(fgetl(fr));
+    in_val = str2num(fgetl(fr));
+    out_val = str2num(fgetl(fr));
+    r = sim(net, in_val');
+    if (out_val != round(r))
+      err++;
+    endif
   endfor
   fclose(fr);
-  out_val = out_val';
-  in_val = in_val';
+  
+  printf("%d errors of %d examples: %f %% accuraccy\n", err, filedesc(1),
+    (1 - (err / filedesc(1))) * 100);
 
 endfunction
--- net_train.m
+++ net_train.m
@@ -4,12 +4,12 @@ function net = net_train(ifile)
   filedesc = str2num(fgetl(fr));
   # filedesc is vector of [number_of_examples input_dimension output_dimension]
   for i = 1:filedesc(1)
-    in_val(i,:) = str2num(fgetl(fr));
-    out_val(i,:) = str2num(fgetl(fr));
+    in_val(i, :) = str2num(fgetl(fr));
+    out_val(i, :) = str2num(fgetl(fr));
   endfor
   fclose(fr);
-  out_val = out_val';
   in_val = in_val';
+  out_val = out_val';
   limits = min_max(in_val);
   topology = [2 2 1];
   net = newff(limits, topology, {"tansig", "tansig", "purelin"},
